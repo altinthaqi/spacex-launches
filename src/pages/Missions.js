6be5c8f7fc9113fcda4bb410a58443af2675ecd9
altinthaqi <altin.thaqi.520@gmail.com>
2022-01-18 22:48:11 +0100

    Set up Apollo Client

---
+++ src/pages/Missions.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { useQuery } from "@apollo/client";
+
+// {
+//     launchesPast(limit: 1) {
+//       mission_name
+//       launch_site {
+//         site_name_long
+//       }
+//       links {
+//         flickr_images
+//       }
+//       rocket {
+//         rocket {
+//           id
+//           name
+//         }
+//       }
+//       launch_year
+//       details
+//     }
+//   }
+
+function Missions() {
+  return <>mission</>;
+}
+
+export default Missions;
